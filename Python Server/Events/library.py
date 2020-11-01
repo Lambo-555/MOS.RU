617074c99dda760bed37026f085d7ecf2cdb5abb
M1STYNEBULA <mistynebula@yandex.ru>
2020-11-01 07:29:41 +0300

    DELETED

--- Python Server/Events/library.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.simplefilter("ignore", DeprecationWarning)
-
-def build(user_id):               #Функция которая принимает в себя идентификатор пользователя
-    return example_value
